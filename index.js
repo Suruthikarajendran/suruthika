88f1fa5ddb512eea9178f2a8a9480f99031fcf14
Suruthikarajendran <r.sruthi11245@gmail.com>
2025-07-21 15:46:51 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
-var x=3;
-let name ='js';
-const iscloudy=true;
-
-console.log(name+""+iscloudy);
-
-document.write(name);
-document.write(x);
-document.write(iscloudy);
-
-function add(){
-    console.log("this is a function"+(a+b));
+//variables declare outside any function or block are in the global scope.
+//they are accessible anywhere  in the script.
+let x=10;
+function send message(){
+    console.log("Hai Js");
+f    console.log(x);//Inside
 }
-add(4,5);
+sendMessage();
+    console.log(x);//Outside
+
+//function scope:
+//only accessible in the function in which they are declared.    
+function send message(){
+    console.log("Hai Js");
+f    console.log(x);//Inside
+}
+sendMessage();
+    console.log(x);//Outside
+
+//block scope
+//The block scope of the variable means that the variable
+//is accessible within the block that is between the curly braces.
+function check{
+    if(true){
+        let name="webdevelopment";
+        console.log(name);
+    }
+    console.log(name);
+}
+check()
+    
+//call back;
+//A callback function in Javascript is a function that is passed as an argument to another function,
+//and it is executed after the completion of that function.
+
+//it is commonly used for:
+//Handling asynchronous operations(like API calls,timeouts,or event handling)
+//Customizing behaviour inside function.
+function first(){
+    console.log("This is my first function")
+}
+first()
+function Second(callback){
+    console.log("This is my second function")
+    callback();
+}
+second(first)
+
+ 
+
+
