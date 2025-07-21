438e7219dfef5f943859c71fec7fd9c7baaf3196
Suruthikarajendran <r.sruthi11245@gmail.com>
2025-07-21 15:43:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var x=3;
+let name ='js';
+const iscloudy=true;
+
+console.log(name+""+iscloudy);
+
+document.write(name);
+document.write(x);
+document.write(iscloudy);
+
+function add(){
+    console.log("this is a function"+(a+b));
+}
+add(4,5);
