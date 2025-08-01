0b5233df256e843b5bff9293a72a5777ffe01341
Suruthikarajendran <r.sruthi11245@gmail.com>
2025-08-01 14:10:00 +0530

    Task4.c

---
+++ Task4.c
@@ -0,0 +1,7 @@
+function reverseWords(sentence) {
+  return sentence.split(' ').reverse().join(' ');
+}
+
+// Example
+console.log(reverseWords("Hello world from ChatGPT"));  
+// Output: "ChatGPT from world Hello"
