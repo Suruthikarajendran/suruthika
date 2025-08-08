e11548bcc796508f27a33b8a45f6c41fd8d9baf4
Suruthikarajendran <r.sruthi11245@gmail.com>
2025-08-08 15:41:38 +0530

    Create day8.js

---
+++ day8.js
@@ -0,0 +1,32 @@
+let current=new Date();
+let day=current.getDay();
+console.log(current)
+console.log(current.toString());
+console.log(day);
+class Sample{
+    constructor(personname){
+        this.personname=personname;
+        console.log(personname);
+    }
+}
+let result=new Sample("js");
+
+
+class Bank{
+    constructor(initialamount=0){
+        this._balance=initialamount;
+    }
+    get balance(){
+        return this._balance;
+    }
+    set balance(amount){
+        if(amount>0){
+            this._balance+=amount;
+            console.log(amount);
+        }
+    }
+}
+let b=new Bank(10);
+console.log(b)
+b.balance=100;
+console.log(b)
